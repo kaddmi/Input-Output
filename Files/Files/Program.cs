426aec2a13a97489efbcf14b5a1605e4e2e5f93e
Anastasija Pischik <nastijpischik@gmail.com>
2018-09-22 15:33:16 +0300

    in-out

---
+++ Files/Files/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter path");
+            string path = Console.ReadLine();
+            try
+            {
+                List<string> filesName = new List<string>();
+                filesName = Directory.GetFiles(path).ToList<string>();
+                Console.WriteLine("Files in directory:");
+                foreach (string file in filesName)
+                {
+                    Console.WriteLine(file);
+                }
+                Console.WriteLine("Enter name of file");
+                string chosFile = Console.ReadLine();
+                StreamReader streamReader = new StreamReader(path + chosFile);
+                string line;
+                line = streamReader.ReadLine();
+                while (line != null)
+                {
+                    Console.WriteLine(line);
+                    line = streamReader.ReadLine();
+                }
+                streamReader.Close();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }     
+            Console.ReadLine();
+        }
+    }
+}
